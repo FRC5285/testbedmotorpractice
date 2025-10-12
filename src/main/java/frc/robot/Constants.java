933d5b438ad7a8251d0e505f076f6b1408f15b7f
Saam Mazaheri <mazaheri.saam@gmail.com>
2025-10-12 15:03:52 -0700

    FINAL FIX 3

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,14 +2,20 @@ package frc.robot;
 
 public final class Constants {
 
-    public static final class MotorConstants {
-        public static final int motorCanId = 1;
+    // Operator/controller constants
+    public static final class OperatorConstants {
+        public static final int kDriverControllerPort = 0; // USB port of your joystick/gamepad
+    }
 
-        // PID constants
-        public static final double kP = 0.1;
+    // Motor constants
+    public static final class MotorConstants {
+        public static final int kMotorCanId = 1; // CAN ID of your Talon FX
+        public static final double kP = 0.1;    // PID constants
         public static final double kI = 0.0;
         public static final double kD = 0.0;
     }
 }
+
+
 
 
