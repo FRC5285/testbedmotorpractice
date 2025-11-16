9f8ec9572f2e47757dba8151fb08f196b01603f1
eatean <ethanyangtaco115@gmail.com>
2025-11-16 14:33:33 -0800

    new values + new writing

--- src/main/java/frc/robot/subsystems/MotorSubsystem.java
+++ src/main/java/frc/robot/subsystems/MotorSubsystem.java
@@ -6,10 +6,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.MotorConstants;
 
-import static edu.wpi.first.units.Units.Rotations;
-import static edu.wpi.first.units.Units.RotationsPerSecond;
-import static edu.wpi.first.units.Units.RotationsPerSecondPerSecond;
-import static edu.wpi.first.units.Units.Second;
 
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
@@ -28,22 +24,22 @@ public class MotorSubsystem extends SubsystemBase {
     public MotorSubsystem() {
         TalonFXConfiguration configs = new TalonFXConfiguration();
         MotionMagicConfigs mm = new MotionMagicConfigs();
-        mm.MotionMagicCruiseVelocity = 15.0;   
-        mm.MotionMagicAcceleration = 25.0;    
-        mm.MotionMagicJerk = 10.0;
+        mm.MotionMagicCruiseVelocity = 80;   
+        mm.MotionMagicAcceleration = 160;    
+        mm.MotionMagicJerk = 1600;
         configs.MotionMagic = mm;
 
         Slot0Configs slot0 =configs.Slot0;
-        slot0.kS = 0.08;
-        slot0.kV = 0.11;
+        slot0.kS = 0.25;
+        slot0.kV = 0.12;
         slot0.kA = 0.01;
-        slot0.kP = 0.7;
+        slot0.kP = 4.8;
         slot0.kI = 0.0;
-        slot0.kD = 0.4;
+        slot0.kD = 0.1;
         configs.Slot0 = slot0;
 
         configs.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-        motor.setPosition(0);
+
         motor.getConfigurator().apply(configs);
         }
 
@@ -67,6 +63,7 @@ public class MotorSubsystem extends SubsystemBase {
     public void periodic() {
         double currentPos = motor.getPosition().getValueAsDouble();
 
+        if (Math.abs(currentPos - targetPosition) < tolerance) {
             if (pendingTurns > 0) {
                 pendingTurns--;
                 targetPosition += 1.0;
@@ -74,13 +71,9 @@ public class MotorSubsystem extends SubsystemBase {
                 pendingTurns++;
                 targetPosition -= 1.0;
             }
-        
-
-        motor.setControl(motionMagicRequest.withPosition(targetPosition));
-
-        if ((Math.abs(currentPos - targetPosition)) < tolerance) {
-            stopMotor();
         }
+
+        motor.setControl(motionMagicRequest.withPosition(targetPosition).withSlot(0));
         // check if motor reached the target within tolerance
         SmartDashboard.putNumber("rotations", currentPos);
         SmartDashboard.putNumber("traget", targetPosition);
