e96addd2b953111c2e73881622d5fa7aa2e47688
eatean <ethanyangtaco115@gmail.com>
2026-01-12 18:30:00 -0800

    i did something bad

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import frc.robot.subsystems.MotorSubsystem;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
