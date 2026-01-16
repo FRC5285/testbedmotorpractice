0ae59186b5211683768cc733cb79ba56285d91da
ethan <ethan577404@gmail.com>
2026-01-15 19:36:34 -0800

    fahhh

--- src/main/java/frc/robot/subsystems/MotorSubsystem.java
+++ src/main/java/frc/robot/subsystems/MotorSubsystem.java
@@ -38,10 +38,10 @@ public class MotorSubsystem extends SubsystemBase {
 
         thisMotor.setPosition(0);
 
-        motorPID.setTolerance(0.01);
+        motorPID.setTolerance(0.06);
 
         m_encoder.reset();
-        m_encoder.setDistancePerPulse(1.0 / 124.0);
+        m_encoder.setDistancePerPulse(1.0 / 248.0);
 
         // Telemetry
         SendableRegistry.add(this, "Motor");
