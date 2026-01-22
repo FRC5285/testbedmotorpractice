29c325ac7fb95240bb27715f3c3a8e86102dd192
ethan <ethan577404@gmail.com>
2026-01-21 18:09:56 -0800

    test

--- src/main/java/frc/robot/ledConstants.java
+++ src/main/java/frc/robot/ledConstants.java
@@ -44,7 +44,7 @@ public final class ledConstants {
 
         //pride flag presets
         public static final double[] trans_flag = {
-        1.0, skyBlue, hotPink, white, hotPink, skyBlue
+        1.0, skyBlue, red, white, orange, violet
         };
 
 
