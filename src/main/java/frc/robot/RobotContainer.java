8b2244b6d42ae76c1c6be58c20b68ede57d83302
eatean <ethanyangtaco115@gmail.com>
2026-03-16 20:10:05 -0700

    fix sys id

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,45 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
-import frc.robot.subsystems.MotorSubsystem;
-import frc.robot.subsystems.ledSubSystem;
+import com.ctre.phoenix6.SignalLogger;
+
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.subsystems.FlywheelMechanism;
 
-import static frc.robot.ledConstants.Constants.*;
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
 public class RobotContainer {
     private final CommandXboxController m_joystick = new CommandXboxController(0);
+    private final FlywheelMechanism m_mechanism = new FlywheelMechanism();
 
-    // The robot's subsystems and commands are defined here...
-    private final MotorSubsystem Motor = new MotorSubsystem();
-    private final ledSubSystem theLED = new ledSubSystem();
-
-    // Xbox Controller Object
-    /** The container for the robot. Contains subsystems, OI devices, and commands. */
     public RobotContainer() {
-        // Configure the trigger bindings
         configureBindings();
-        theLED.currentPattern = trans_flag; //first index is speed in Hz
     }
 
-    /**
-     * Use this method to define your trigger->command mappings. Triggers can be created via the
-     * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-     * predicate, or via the named factories in {@link
-     * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-     * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-     * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-     * joysticks}.
-     */
     private void configureBindings() {
-        // When "b" is pressed on the controller, turn the motor clockwise 360 degrees.
-        m_joystick.leftBumper().whileTrue(Motor.runmotor());
-        m_joystick.leftBumper().whileFalse(Motor.stopMotor());
+        /* Default command is duty cycle control with the left up/down stick */
+        m_mechanism.setDefaultCommand(m_mechanism.joystickDriveCommand(m_joystick::getLeftY));
+
+        /* Manually start logging with left bumper before running any tests,
+         * and stop logging with right bumper after we're done with ALL tests.
+         * This isn't necessary but is convenient to reduce the size of the hoot file */
+        m_joystick.leftBumper().onTrue(Commands.runOnce(SignalLogger::start));
+        m_joystick.rightBumper().onTrue(Commands.runOnce(SignalLogger::stop));
+
+        /*
+         * Joystick Y = quasistatic forward
+         * Joystick A = quasistatic reverse
+         * Joystick B = dynamic forward
+         * Joystick X = dyanmic reverse
+         */
+        m_joystick.y().whileTrue(m_mechanism.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
+        m_joystick.a().whileTrue(m_mechanism.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
+        m_joystick.b().whileTrue(m_mechanism.sysIdDynamic(SysIdRoutine.Direction.kForward));
+        m_joystick.x().whileTrue(m_mechanism.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+    }
+
+    public Command getAutonomousCommand() {
+        return Commands.print("No autonomous command configured");
     }
 }
